94354e919790c9468a9a7c9c12b04be4eb3b638a
Eduardo Neiva <orlekc@gmail.com>
2013-05-21 00:23:35 +1000

    getisilhouette
    getsilhouette

--- project/+spacecarving/getsilhouette.m
+++ project/+spacecarving/getsilhouette.m
@@ -1,25 +1,13 @@
-function S = getsilhouette( im, SI)
-% %GETSILHOUETTE - find the silhouette of an object centered in the image
-% 
-% %   Copyright 2005-2009 The MathWorks, Inc.
-% %   $Revision: 1.0 $    $Date: 2006/06/30 00:00:00 $
-% %close all;
-% %im  = cameras(1).Image;
-% [h,w,d] = size(im);
-% 
-% v = rgb2gray(im);
-% 
-% % Initial segmentation based on more red than blue
-% S = v(:,:,1) < (200);
-% 
-% % Remove regions touching the border or smaller than 10% of image area
-% S = imclearborder(S);
-% S = bwareaopen(S, ceil(h*w/10));
-% 
-% % Now remove holes < 1% image area
-% S = ~bwareaopen(~S, ceil(h*w/100));
-% 
-% %imshow(S);
+function S = getsilhouette(ii, Silhouettes)
+% getsilhouette(ii, SI):
+%    Return the ii-th silhouette image
+%
+% ARGUMENTS:
+% II = Silhouette index
+% SILHOUETTES = Silhouettes struct
+%
+% RETURNS:
+% S = ii-th silhouette image
+%
 
-
-S = SI(im).si;
+S = Silhouettes(ii).si;
