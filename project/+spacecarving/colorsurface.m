2f786cdf91e818e115603e04cc29017d950c37a6
Thiago <tmeloliveira@gmail.com>
2013-05-21 00:27:36 +1000

    Fixing colorsurface

--- project/+spacecarving/colorsurface.m
+++ project/+spacecarving/colorsurface.m
@@ -21,7 +21,7 @@ for ii=1:num_cam
 end
 
 % Use the normal to find the best camera and view the value.
-v_data = zeros( num_vert, 3 );
+vertexcdata = zeros( num_vert, 3 );
 for ii=1:num_vert
     % Find the best camera
     angles = normals(ii,:)*cam_normals./norm(normals(ii,:));
@@ -29,8 +29,8 @@ for ii=1:num_vert
     % And projects the vertex into the chosen one
     [imx,imy] = spacecarving.project( cameras(cam_idx), ...
         vert(ii,1), vert(ii,2), vert(ii,3) );
-    v_data(ii,:) = double( cameras(cam_idx).Image( round(imy), round(imx), : ) )/255;
+    vertexcdata(ii,:) = double( cameras(cam_idx).Image( round(imy), round(imx), : ) )/255;
 end
 
 % Set it into the patch
-set( ptch, 'Facev_data', v_data, 'FaceColor', 'interp' );
+set( ptch, 'Facevertexcdata', vertexcdata, 'FaceColor', 'interp' );
