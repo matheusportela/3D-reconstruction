cfd047adbefa03e3cd5d4938c01f97d65adf5ffc
Eduardo Neiva <orlekc@gmail.com>
2013-05-20 23:48:17 +1000

    rvert

--- project/+spacecarving/colorsurface.m
+++ project/+spacecarving/colorsurface.m
@@ -1,34 +1,35 @@
 function colorsurface( ptch, cameras )
-%COLORSURFACE: color in a surface based on image data
+% colorsurface( ptch, cameras )
+%   This function 'paints' the 3D model based on the colors from the
+%   original images. It uses the nearest image pixel to get the color.
 %
-%   COLORSURFACE(PTCH,CAMERAS) colors-in each vertex of the patch PTCH
-%   using the nearest image pixel in one of the cameras in CAMERAS.
+% ARGUMENTS:
+% PTCH = this argument contains the 3D model to be colored. 
+% CAMERAS = this argument contains the images from which the colors are
+% extracted.
+%
 
-%   Copyright 2005-2009 The MathWorks, Inc.
-%   $Revision: 1.0 $    $Date: 2006/06/30 00:00:00 $
-
-
-vertices = get( ptch, 'Vertices' );
+vert = get( ptch, 'Vertices' );
 normals = get( ptch, 'VertexNormals' );
-num_vertices = size( vertices, 1 );
+num_vert = size( vert, 1 );
 
 % Get the view vector for each camera
-num_cameras = numel( cameras );
-cam_normals = zeros( 3, num_cameras );
-for ii=1:num_cameras
+num_cam = numel( cameras );
+cam_normals = zeros( 3, num_cam );
+for ii=1:num_cam
     cam_normals(:,ii) = spacecarving.getcameradirection( cameras(ii) );
 end
 
 % For each vertex, use the normal to find the best camera and then lookup
 % the value.
-vertexcdata = zeros( num_vertices, 3 );
-for ii=1:num_vertices
+vertexcdata = zeros( num_vert, 3 );
+for ii=1:num_vert
     % Use the dot product to find the best camera
     angles = normals(ii,:)*cam_normals./norm(normals(ii,:));
     [~,cam_idx] = min( angles );
     % Now project the vertex into the chosen camera
     [imx,imy] = spacecarving.project( cameras(cam_idx), ...
-        vertices(ii,1), vertices(ii,2), vertices(ii,3) );
+        vert(ii,1), vert(ii,2), vert(ii,3) );
     vertexcdata(ii,:) = double( cameras(cam_idx).Image( round(imy), round(imx), : ) )/255;
 end
 
--- project/+spacecarving/getcameradirection.m
+++ project/+spacecarving/getcameradirection.m
@@ -1,20 +1,31 @@
-function dirn = getcameradirection( camera )
-%GETCAMERADIRECTION: return the view direction of a camera
+function direction = getcameradirection(camera)
+% getcameradirection(camera)
+%   This function returns a unitary 3D vector pointing to the direction
+%   focused by camera
 %
-%   DIRN = GETCAMERADIRECTION(CAMERA) returns a unit vector giving the
-%   direction from the camera centre along the camera principal axis.
+%
+% ARGUMENTS:
+% CAMERA = Camera struct
+%
+%
+% RETURNS:
+% DIRECTION = Unitary vector
+%
 
-%   Copyright 2005-2009 The MathWorks, Inc.
-%   $Revision: 1.0 $    $Date: 2006/06/30 00:00:00 $
+% Image sizes in the X and Y axes
+size_x = size(camera.Image, 2);
+size_y = size(camera.Image, 1);
 
+% Homogeneous centre point of the image
+centre = [size_x/2
+          size_y/2
+          1.0];
 
-% First, get the centre in image coordinates
-x = [
-    size( camera.Image, 2 ) / 2
-    size( camera.Image, 1 ) / 2
-    1.0
-    ];
+% Project point to 3D
+X = camera.K\centre; % equivalent to inv(camera.K)*centre
 
-X = camera.K \ x;
+% Apply rotation
 X = camera.R'*X;
-dirn = X ./ norm( X );
+
+% Normalise to transform to 3D
+direction = X./norm(X);
