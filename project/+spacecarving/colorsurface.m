b6f1a7c7f7028f65acf2ac89dc6d2e9d29cc84fe
Thiago <tmeloliveira@gmail.com>
2013-05-21 00:20:46 +1000

    colorsurface

--- project/+spacecarving/colorsurface.m
+++ project/+spacecarving/colorsurface.m
@@ -13,25 +13,24 @@ vert = get( ptch, 'Vertices' );
 normals = get( ptch, 'VertexNormals' );
 num_vert = size( vert, 1 );
 
-% Get the view vector for each camera
+% For each camera, get the view vector
 num_cam = numel( cameras );
 cam_normals = zeros( 3, num_cam );
 for ii=1:num_cam
     cam_normals(:,ii) = spacecarving.getcameradirection( cameras(ii) );
 end
 
-% For each vertex, use the normal to find the best camera and then lookup
-% the value.
-vertexcdata = zeros( num_vert, 3 );
+% Use the normal to find the best camera and view the value.
+v_data = zeros( num_vert, 3 );
 for ii=1:num_vert
-    % Use the dot product to find the best camera
+    % Find the best camera
     angles = normals(ii,:)*cam_normals./norm(normals(ii,:));
     [~,cam_idx] = min( angles );
-    % Now project the vertex into the chosen camera
+    % And projects the vertex into the chosen one
     [imx,imy] = spacecarving.project( cameras(cam_idx), ...
         vert(ii,1), vert(ii,2), vert(ii,3) );
-    vertexcdata(ii,:) = double( cameras(cam_idx).Image( round(imy), round(imx), : ) )/255;
+    v_data(ii,:) = double( cameras(cam_idx).Image( round(imy), round(imx), : ) )/255;
 end
 
 % Set it into the patch
-set( ptch, 'FaceVertexCData', vertexcdata, 'FaceColor', 'interp' );
+set( ptch, 'Facev_data', v_data, 'FaceColor', 'interp' );
